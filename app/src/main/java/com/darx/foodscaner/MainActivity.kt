4b76a72731a1101c8628162a4c69eab5f5ba7f4b
Moleque <molecada@yandex.ru>
2019-09-24 09:53:13 +0300

    Added fragments

--- app/src/main/java/com/darx/foodscaner/MainActivity.kt
+++ app/src/main/java/com/darx/foodscaner/MainActivity.kt
@@ -2,11 +2,20 @@ package com.darx.foodscaner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.darx.foodscaner.fragments.Adapter
+import com.darx.foodscaner.fragments.CameraFragment
+import com.darx.foodscaner.fragments.InfoFragment
+import com.darx.foodscaner.fragments.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewPager.adapter = Adapter()
     }
 }
---
+++ app/src/main/java/com/darx/foodscaner/fragments/PagerAdapter.kt
@@ -0,0 +1,14 @@
+package com.darx.foodscaner.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+    override fun getItem(position: Int): Fragment {
+
+    }
+
+    get
+}

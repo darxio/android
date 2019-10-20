d9ad5204e402a947a3202d35a53bb6fc93404673
Moleque <molecada@yandex.ru>
2019-10-20 17:46:49 +0300

    Created new views

--- app/src/main/java/com/darx/foodscaner/FavoritesActivity.kt
+++ app/src/main/java/com/darx/foodscaner/FavoritesActivity.kt
@@ -8,6 +8,7 @@ import com.darx.foodscaner.adapters.ProductAdapter
 import com.darx.foodscaner.data.response.Group
 import com.darx.foodscaner.data.response.Product
 import com.darx.foodscaner.services.LocalDatabase
+import kotlinx.android.synthetic.main.activity_favorites.*
 import kotlinx.android.synthetic.main.activity_groups.*
 import java.io.Serializable
 
@@ -19,7 +20,18 @@ class FavoritesActivity : AppCompatActivity() {
 
         val database = LocalDatabase(applicationContext)
         val productDao = database.productDao()
-        val fovoriteProducts = productDao.getProducts()
+        val fovoriteProducts = listOf<Product>(
+            Product(1, "Сникерс", 13443, "Вкусно"),
+            Product(1, "Сникерс", 13443, "Вкусно"),
+            Product(1, "Сникерс", 13443, "Вкусно"),
+            Product(1, "Сникерс", 13443, "Вкусно"),
+            Product(1, "Сникерс", 13443, "Вкусно"),
+            Product(1, "Сникерс", 13443, "Вкусно"),
+            Product(1, "Сникерс", 13443, "Вкусно"),
+            Product(1, "Сникерс", 13443, "Вкусно")
+        )
+
+//                = productDao.getProducts()
 
         val productAdapter = ProductAdapter(fovoriteProducts as List<Product>, object : ProductAdapter.Callback {
             override fun onItemClicked(item: Product) {
@@ -28,6 +40,6 @@ class FavoritesActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         })
-        groupRecycler.adapter = productAdapter
+        productRecycler.adapter = productAdapter
     }
 }

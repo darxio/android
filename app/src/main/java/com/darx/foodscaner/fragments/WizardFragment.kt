dcab526d853d31698475f6e76caf9d3b7240f680
ksgl <ksenia.glazacheva@gmail.com>
2019-10-25 23:10:48 +0300

    works

--- app/src/main/java/com/darx/foodscaner/fragments/WizardFragment.kt
+++ app/src/main/java/com/darx/foodscaner/fragments/WizardFragment.kt
@@ -1,24 +1,12 @@
 package com.darx.foodscaner.fragments
 
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_profile.*
-import kotlinx.android.synthetic.main.fragment_profile.view.*
-import androidx.lifecycle.Observer
 import com.darx.foodscaner.*
-import com.darx.foodscaner.data.request.LoginRqst
-import com.darx.foodscaner.data.request.RegistrationRqst
-import com.darx.foodscaner.services.ApiService
-import com.darx.foodscaner.services.ConnectivityInterceptorImpl
-import com.darx.foodscaner.services.NetworkDataSourceImpl
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class WizardFragment : Fragment() {
 

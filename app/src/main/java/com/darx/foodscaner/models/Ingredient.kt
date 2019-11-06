637c3b655f13d3c03a85eb4cab5c2308df34207d
Artem <mouseartiom@yandex.ru>
2019-11-06 21:20:56 +0300

    fix needed

--- app/src/main/java/com/darx/foodscaner/models/Ingredient.kt
+++ app/src/main/java/com/darx/foodscaner/models/Ingredient.kt
@@ -1,16 +1,4 @@
 package com.darx.foodscaner.models
 
-data class Ingredient (
-    val name: String,
-    val danger: Int = -1,
-    val ingredients: ArrayList<Ingredient> = ArrayList<Ingredient>()
-
-)
-
-data class IngredientExtended (
-    val name: String,
-    val danger: Int,
-    val description: String,
-    val wiki_link: String,
-    val ingredients: Ingredient
-)
+class Ingredient {
+}

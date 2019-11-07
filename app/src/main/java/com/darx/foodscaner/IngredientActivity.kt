101847eb4c79071075c38a6c87b4395de7cc46e4
Moleque <molecada@yandex.ru>
2019-11-07 04:00:42 +0300

    Add searching for local user data

--- app/src/main/java/com/darx/foodscaner/IngredientActivity.kt
+++ app/src/main/java/com/darx/foodscaner/IngredientActivity.kt
@@ -18,7 +18,6 @@ class IngredientActivity : AppCompatActivity() {
     private lateinit var ingredientToShow: IngredientModel
     private var isExcept: Boolean = false
 
-    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ingredient)
@@ -32,15 +31,10 @@ class IngredientActivity : AppCompatActivity() {
         ingredientToShow = intent.extras.get("INGREDIENT") as IngredientModel
         collapsingToolbar.title = ingredientToShow.name
         infoIngredient.text = ingredientToShow.description
-//        collapsingToolbar.background = R.drawable.ingredient.toDrawable() IMAGE
-
+        // collapsingToolbar.background = R.drawable.ingredient.toDrawable() IMAGE
 
         ingredientViewModel?.getOne_(ingredientToShow.id)?.observe(this@IngredientActivity, object : Observer<IngredientModel> {
             override fun onChanged(t: IngredientModel?) {
-//                if (t?.id == ingredientToShow.id) {
-//                    exceptingButton.text = "Добавить"
-//                    exceptingButton.setBackgroundColor(R.color.strongPositiveColor)
-//                }
                 if (t?.id == ingredientToShow.id) {
                     exceptingButton.text = resources.getString(R.string.add_ingredient)
                     exceptingButton.setBackgroundColor(resources.getColor(R.color.strongPositiveColor))

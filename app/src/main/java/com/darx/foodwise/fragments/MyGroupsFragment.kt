8384c064a85734bbf080564f6d7d685b60321bd8
Ksyuch <ksenia.glazacheva@gmail.com>
2019-12-07 01:02:19 +0300

    collapsing picture fixed

--- app/src/main/java/com/darx/foodwise/fragments/MyGroupsFragment.kt
+++ app/src/main/java/com/darx/foodwise/fragments/MyGroupsFragment.kt
@@ -37,7 +37,7 @@ class MyGroupsFragment(val groupViewModel: GroupViewModel) : Fragment() {
                 intent.putExtra("GROUP", item as Serializable)
                 startActivity(intent)
             }
-        }, 0, 60, context!!)
+        }, 0, 90, context!!)
         val myGroupsRecycler = view.findViewById<RecyclerView>(R.id.groups_rv)
         myGroupsRecycler.adapter = myGroupAdapter
 
